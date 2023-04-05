bc318f6356a970467cf1b8d7edb405025154fe09
BrunoScholze2003 <104472351+BrunoScholze2003@users.noreply.github.com>
2023-04-05 00:26:33 -0300

    Add files via upload

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -11,40 +11,42 @@ export class CardComponent {
 
   public loading = true;
   public atributoSelecionado!: number;
-
-  constructor() {
-    console.log(this.deckDeCartas);
-    
-  }
-
-  ngOnInit(): void {
-    this.sortearCartas();
-  }
-
-  public deckDeCartas = cartas;
+  public BaralhoCompleto = cartas;
   public deckDoJogardor: Array<cardInterface> = []
   public deckDoRobo: Array<cardInterface> = []
 
 
-  public carta = this.deckDeCartas[0];
+  public carta = this.BaralhoCompleto[0]; // carta do thos fixa no momento
 
-  /*sortearCartas(){
-      this.deckDoJogardor = this.deckDeCartas[this.indiceAleatorio];
-      this.deckDoRobo = this.deckDeCartas[this.indiceAleatorio];
-    console.log(this.deckDoJogardor);
-    console.log(this.deckDoRobo);
+	constructor() {
+   		 console.log(this.BaralhoCompleto);
+  	}
+
+    ngOnInit(): void {
+    	this.destribuidorDeCartasJogador();
+  	}
+
+ 
+
+  	destribuidorDeCartasJogador(){
+    	const cartaDistribuida: Array<Number> = [];
     
-  }*/
+		for(let i = 0; i <= 2; i ++){
+			const indiceAleatorio = Math.floor(Math.random() * this.BaralhoCompleto.length);
 
-  sortearCartas(){
-    for(let i = 0; i <= 5; i ++){
-      const indiceAleatorio = Math.floor(Math.random() * this.deckDeCartas.length);
-      const cartaAleatoria = this.deckDeCartas[indiceAleatorio]; 
-      this.deckDoJogardor.push(cartaAleatoria);
-    }
-
-      console.log(this.deckDoJogardor)
-  }
+			if (cartaDistribuida.includes(indiceAleatorio)) { 
+				i = i - 1;
+				continue;
+			} 
+			else {
+				const cartaAleatoria = this.BaralhoCompleto[indiceAleatorio]; 
+				this.deckDoJogardor.push(cartaAleatoria);
+				cartaDistribuida.push(indiceAleatorio);   
+			} 
+			console.log(cartaDistribuida)
+		}
+		console.log(this.deckDoJogardor) 
+   }
 
   selecionarAtributo(index: number): void {
     this.atributoSelecionado = index;
